37745f0fac6e942cb093f2a06f8a8a8e6cb0c8af
Alex Zhulai <Lepiozdyx@gmail.com>
2023-06-12 11:11:13 -0300

    Initial Commit

---
+++ ColorMixerSwiftUI/ColorMixerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixerSwiftUIApp.swift
+//  ColorMixerSwiftUI
+//
+//  Created by Alex on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorMixerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
